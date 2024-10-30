ff6940dbf1b770e2a9a6f86791a3be617d40c195
Ahmednizami <muhammadahmedsaeednizami@gmail.com>
2024-10-30 15:34:43 +0500

    Version Update From 0.1.3 to 0.1.4

--- src/casting_expert/__init__.py
+++ src/casting_expert/__init__.py
@@ -5,8 +5,9 @@ This package provides robust tools for handling type conversions,
 with built-in validation and error handling.
 """
 
-from .core import safe_cast, cast_to_type
 from .validators import validate_input
+from .core import safe_cast, cast_to_type
+from .str_to_dict.parsers import parse_string_to_dict, ParsingError
 
-__version__ = "0.1.0"
-__all__ = ['safe_cast', 'cast_to_type', 'validate_input']
+__version__ = "0.1.4"
+__all__ = ['safe_cast', 'cast_to_type', 'validate_input', 'parse_string_to_dict', 'ParsingError']
